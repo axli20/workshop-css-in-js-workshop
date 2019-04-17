e738c48c020b0fda1e1b66838778ae8f05f01179
Angela Li <alidartmouth20@gmail.com>
2019-04-17 17:27:00 -0400

    finished tutorial

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR API KEY'; // add your own key here
+const API_KEY = 'AIzaSyCw70sRbaNZBJoNT2jVk8cOv2M9nUt2VwM'; // add your own key here
 
 const youtubeSearch = (term) => {
   const params = {
